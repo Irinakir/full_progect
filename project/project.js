a166120d40b5ff64f86739a6a749a4d2c518b10f
Irina <kirillova_irina@ukr.net>
2018-12-17 10:42:23 +0200

    Update project.js

--- project/project.js
+++ project/project.js
@@ -7,3 +7,24 @@ $(".product").on('click', function() {
     $('#popup-container').css('display', 'none');
     }
     });
+
+$(".prod_img").on('click', function() {
+  //$('.new_prod_part').click(function(){
+    $('#popup-container').css('display', 'flex');
+  });
+  $('#popup-container').click(function(e){
+    if(this===e.target){
+    $('#popup-container').css('display', 'none');
+    }
+    });
+
+$(".new_prod_part-img").on('click', function() {
+  //$('.new_prod_part').click(function(){
+    $('#popup-container').css('display', 'flex');
+  });
+  $('#popup-container').click(function(e){
+    if(this===e.target){
+    $('#popup-container').css('display', 'none');
+    }
+    });
+
